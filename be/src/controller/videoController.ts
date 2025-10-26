e6f9baf60023861862a37196a1b2c956cffb3f63
Akshay-Reddy00 <akshayreddym19@gmail.com>
2025-10-26 19:30:11 +0530

    youtube video fetch - be

--- be/src/controller/videoController.ts
+++ be/src/controller/videoController.ts
@@ -1,6 +1,58 @@
 // src/controllers/videoController.ts
 import { Request, Response } from 'express';
+import axios from "axios";
 import { Video } from '../models/video';
+
+interface YoutubeResponse {
+    title: string;
+    author_name: string;
+    thumbnail_url: string;
+    html: string;
+
+}
+
+export const getPublicVideoDetails = async (req: Request, res: Response) => {
+    try {
+        const {url} = req.query;
+        if(!url || typeof url !== 'string') {
+            return res.status(400).json({
+                message: "Youtube url is required"
+            })
+        }
+
+        const embedUrl = `https://www.youtube.com/oembed?url=${encodeURIComponent(url)}&format='json`;
+        const response = await axios.get<YoutubeResponse>(embedUrl);
+        const data = response.data;
+
+        const videoIdMatch = url.match(/v=([^&]+)/);
+        const videoId = videoIdMatch ? videoIdMatch[1] : null;
+
+        if(videoId) {
+            await Video.findOneAndUpdate( {videoId}, {title: data.title, description: data.author_name});
+        }
+
+        res.status(200).json({
+            videoId,
+            title: data.title,
+            author: data.author_name,
+            thumbnail: data.thumbnail_url,
+            embedHtml: data.html,
+            videoUrl: url
+        })
+    } catch(err: any) {
+        res.status(500).json({message: 'Failed to fetch video details'});
+    }
+}
+
+// GET /videos - Get all videos
+export const getVideos = async (req: Request, res: Response) => {
+    try {
+        const videos = await Video.find();
+        res.json(videos);
+    } catch (err) {
+        res.status(500).json({ message: 'Server error', error: err });
+    }
+};
 
 // GET /videos/:id - Get video details
 export const getVideo = async (req: Request, res: Response) => {
--- be/src/routes/videoRoute.ts
+++ be/src/routes/videoRoute.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 const router = express.Router();
 import { requireAuth } from '../middleware/authMiddleware';
-import { getVideo, addVideo, updateVideo } from '../controller/videoController';
+import { getVideos, getVideo, addVideo, updateVideo, getPublicVideoDetails } from '../controller/videoController';
 
+router.get('/public/details', requireAuth, getPublicVideoDetails)
+router.get('/', requireAuth, getVideos);           // Get all videos
 router.get('/:id', requireAuth, getVideo);         // Get video details
 router.post('/', requireAuth, addVideo);           // Add a new video
 router.put('/:id', requireAuth, updateVideo);      // Update title/description
